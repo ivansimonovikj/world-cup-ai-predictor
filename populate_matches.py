15e24513f60743d279e9908195ccc2f768d061d2
Ivan Simonovik <ivansimonovik@gmail.com>
2026-06-22 10:57:39 +0200

    Fix Bosnia

--- populate_matches.py
+++ populate_matches.py
@@ -10,10 +10,6 @@ print("Previous data wiped clean!")
 
 # 2. The Group Stage Data
 group_stage_data = [
-  {"date": "2026-06-21", "team_a": "Spain", "team_b": "Saudi Arabia", "kickoff_et": "12:00 PM", "stadium": "Atlanta Stadium"},
-  {"date": "2026-06-21", "team_a": "Belgium", "team_b": "Iran", "kickoff_et": "3:00 PM", "stadium": "Los Angeles Stadium"},
-  {"date": "2026-06-21", "team_a": "Uruguay", "team_b": "Cape Verde", "kickoff_et": "6:00 PM", "stadium": "Miami Stadium"},
-  {"date": "2026-06-21", "team_a": "New Zealand", "team_b": "Egypt", "kickoff_et": "9:00 PM", "stadium": "BC Place Vancouver"},
   {"date": "2026-06-22", "team_a": "Argentina", "team_b": "Austria", "kickoff_et": "1:00 PM", "stadium": "Dallas Stadium"},
   {"date": "2026-06-22", "team_a": "France", "team_b": "Iraq", "kickoff_et": "5:00 PM", "stadium": "Philadelphia Stadium"},
   {"date": "2026-06-22", "team_a": "Norway", "team_b": "Senegal", "kickoff_et": "8:00 PM", "stadium": "New York/New Jersey Stadium"},
@@ -23,7 +19,7 @@ group_stage_data = [
   {"date": "2026-06-23", "team_a": "Panama", "team_b": "Croatia", "kickoff_et": "7:00 PM", "stadium": "Toronto Stadium"},
   {"date": "2026-06-23", "team_a": "Colombia", "team_b": "DR Congo", "kickoff_et": "10:00 PM", "stadium": "Guadalajara Stadium"},
   {"date": "2026-06-24", "team_a": "Switzerland", "team_b": "Canada", "kickoff_et": "3:00 PM", "stadium": "BC Place Vancouver"},
-  {"date": "2026-06-24", "team_a": "Bosnia and Herzegovina", "team_b": "Qatar", "kickoff_et": "3:00 PM", "stadium": "Seattle Stadium"},
+  {"date": "2026-06-24", "team_a": "Bosnia & Herzegovina", "team_b": "Qatar", "kickoff_et": "3:00 PM", "stadium": "Seattle Stadium"},
   {"date": "2026-06-24", "team_a": "Scotland", "team_b": "Brazil", "kickoff_et": "6:00 PM", "stadium": "Miami Stadium"},
   {"date": "2026-06-24", "team_a": "Morocco", "team_b": "Haiti", "kickoff_et": "6:00 PM", "stadium": "Atlanta Stadium"},
   {"date": "2026-06-24", "team_a": "Mexico", "team_b": "Czechia", "kickoff_et": "9:00 PM", "stadium": "Mexico City Stadium"},
